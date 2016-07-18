619a827633fe94d2644c06e035411f2fb0486e67
Megan Martinson <hello@meg.codes>
2016-07-18 16:08:47 -0500

    adding the ACTUAL files woops.

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
     //making the button pull a random gif
+    // $('input').
     $('button').on('click', function() {
+      var input = $('#input').val();
+      if(input==null){
         $.get("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC")
             .then(function(response) {
                 var gif = response.data;
@@ -9,5 +12,16 @@ $(document).ready(function() {
                 $('#gifhere').html("<img src=" + gifimg + "></img>");
                 console.log(gifimg);
             });
+          }else{
+            console.log(input);
+            $.get("http://api.giphy.com/v1/gifs/search?q="+ input + "&api_key=dc6zaTOxFJmzC")
+            .then(function(response){
+              var gif = response.data;
+              var gifimg = response.data[0].images.downsized_large.url;
+              $('#gifhere').html("<img src=" + gifimg + "></img>");
+              console.log(response);
+              console.log(gifimg);
+            });
+          };
     });
 });
